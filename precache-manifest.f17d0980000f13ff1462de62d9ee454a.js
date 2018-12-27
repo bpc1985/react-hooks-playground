56bd4353792ca57cb5dfb89c6f71307212dd8ce0
Hung Ho <hung.ho@vertoanalytics.com>
2018-12-27 21:39:04 +0200

    Updates

---
+++ precache-manifest.f17d0980000f13ff1462de62d9ee454a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c57cb355fbdfb1f27871",
+    "url": "/react-hooks-videoplayer/static/js/runtime~main.c57cb355.js"
+  },
+  {
+    "revision": "a95b12920dd514cc0d44",
+    "url": "/react-hooks-videoplayer/static/js/main.a95b1292.chunk.js"
+  },
+  {
+    "revision": "1535914dfd91fe200b07",
+    "url": "/react-hooks-videoplayer/static/js/1.1535914d.chunk.js"
+  },
+  {
+    "revision": "efbcac63803ce9cc00f718a70ab04c9c",
+    "url": "/react-hooks-videoplayer/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-hooks-videoplayer/precache-manifest.f17d0980000f13ff1462de62d9ee454a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-hooks-videoplayer/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
